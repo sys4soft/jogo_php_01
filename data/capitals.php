6b9f42fc04efb18899ea51fa762c46cbad4685e8
Joao Ribeiro <jribeiro@digitalsales.pt>
2023-10-28 23:03:53 +0100

    master

---
+++ data/capitals.php
@@ -0,0 +1,588 @@
+<?php
+return [
+    [
+        "Afeganistão", "Cabul"
+    ],
+    [
+        "África do Sul", "Cidade do Cabo"
+    ],
+    [
+        "Albânia", "Tirana"
+    ],
+    [
+        "Alemanha", "Berlim"
+    ],
+    [
+        "Andorra", "Andorra-a-Velha"
+    ],
+    [
+        "Angola", "Luanda"
+    ],
+    [
+        "Antiga e Barbuda", "São João"
+    ],
+    [
+        "Arábia Saudita", "Riade"
+    ],
+    [
+        "Argélia", "Argel"
+    ],
+    [
+        "Argentina", "Buenos Aires"
+    ],
+    [
+        "Armênia", "Erevã"
+    ],
+    [
+        "Austrália", "Camberra"
+    ],
+    [
+        "Áustria", "Viena"
+    ],
+    [
+        "Azerbaijão", "Baku"
+    ],
+    [
+        "Bahamas", "Nassau"
+    ],
+    [
+        "Bangladexe", "Daca"
+    ],
+    [
+        "Barbados", "Bridgetown"
+    ],
+    [
+        "Barém", "Manama"
+    ],
+    [
+        "Bélgica", "Bruxelas"
+    ],
+    [
+        "Belize", "Belmopan"
+    ],
+    [
+        "Benim", "Porto Novo"
+    ],
+    [
+        "Bielorrússia", "Minsk"
+    ],
+    [
+        "Bolívia", "La Paz"
+    ],
+    [
+        "Bósnia e Herzegovina", "Sarajevo"
+    ],
+    [
+        "Botsuana", "Gaborone"
+    ],
+    [
+        "Brasil", "Brasília"
+    ],
+    [
+        "Brunei", "Bandar Seri Begawan"
+    ],
+    [
+        "Bulgária", "Sófia"
+    ],
+    [
+        "Burquina Faso", "Uagadugu"
+    ],
+    [
+        "Burundi", "Gitega"
+    ],
+    [
+        "Butão", "Timbu"
+    ],
+    [
+        "Cabo Verde", "Praia"
+    ],
+    [
+        "Camarões", "Yaoundé"
+    ],
+    [
+        "Camboja", "Phnom Penh"
+    ],
+    [
+        "Canadá", "Ottawa"
+    ],
+    [
+        "Catar", "Doha"
+    ],
+    [
+        "Cazaquistão", "Astana"
+    ],
+    [
+        "Chade", "Jamena"
+    ],
+    [
+        "Chile", "Santiago"
+    ],
+    [
+        "China", "Pequim"
+    ],
+    [
+        "Chipre", "Nicósia"
+    ],
+    [
+        "Colômbia", "Bogotá"
+    ],
+    [
+        "Comores", "Moroni"
+    ],
+    [
+        "Coreia do Norte", "Pyongyang"
+    ],
+    [
+        "Coreia do Sul", "Seul"
+    ],
+    [
+        "Costa do Marfim", "Iamussucro"
+    ],
+    [
+        "Costa Rica", "São José"
+    ],
+    [
+        "Croácia", "Zagreb"
+    ],
+    [
+        "Cuba", "Havana"
+    ],
+    [
+        "Dinamarca", "Copenhaga"
+    ],
+    [
+        "Dominica", "Roseau"
+    ],
+    [
+        "Egito", "Cairo"
+    ],
+    [
+        "Emirados Árabes Unidos", "Abu Dhabi"
+    ],
+    [
+        "Equador", "Quito"
+    ],
+    [
+        "Eritreia", "Asmara"
+    ],
+    [
+        "Eslováquia", "Bratislava"
+    ],
+    [
+        "Eslovênia", "Liubliana"
+    ],
+    [
+        "Espanha", "Madrid"
+    ],
+    [
+        "Estados Unidos", "Washington, D.C."
+    ],
+    [
+        "Estónia", "Talim"
+    ],
+    [
+        "Etiópia", "Adis Abeba"
+    ],
+    [
+        "Fiji", "Suva"
+    ],
+    [
+        "Filipinas", "Manila"
+    ],
+    [
+        "Finlândia", "Helsínquia"
+    ],
+    [
+        "França", "Paris"
+    ],
+    [
+        "Gabão", "Libreville"
+    ],
+    [
+        "Gâmbia", "Banjul"
+    ],
+    [
+        "Gana", "Acra"
+    ],
+    [
+        "Geórgia", "Tbilisi"
+    ],
+    [
+        "Granada", "São Jorge"
+    ],
+    [
+        "Grécia", "Atenas"
+    ],
+    [
+        "Guatemala", "Cidade da Guatemala"
+    ],
+    [
+        "Guiana", "Georgetown"
+    ],
+    [
+        "Guiné", "Conacri"
+    ],
+    [
+        "Guiné Equatorial", "Malabo"
+    ],
+    [
+        "Guiné-Bissau", "Bissau"
+    ],
+    [
+        "Haiti", "Porto Príncipe"
+    ],
+    [
+        "Holanda", "Amesterdão"
+    ],
+    [
+        "Honduras", "Tegucigalpa"
+    ],
+    [
+        "Hungria", "Budapeste"
+    ],
+    [
+        "Iémen", "Sanaa"
+    ],
+    [
+        "Ilhas Marechal", "Majuro"
+    ],
+    [
+        "Ilhas Salomão", "Honiara"
+    ],
+    [
+        "Índia", "Nova Deli"
+    ],
+    [
+        "Indonésia", "Jacarta"
+    ],
+    [
+        "Irão", "Teerão"
+    ],
+    [
+        "Iraque", "Bagdá"
+    ],
+    [
+        "Irlanda", "Dublin"
+    ],
+    [
+        "Islândia", "Reiquiavique"
+    ],
+    [
+        "Israel", "Jerusalém"
+    ],
+    [
+        "Itália", "Roma"
+    ],
+    [
+        "Jamaica", "Kingston"
+    ],
+    [
+        "Japão", "Tóquio"
+    ],
+    [
+        "Jordânia", "Amã"
+    ],
+    [
+        "Kiribati", "Taraua"
+    ],
+    [
+        "Kuwait", "Cidade do Kuwait"
+    ],
+    [
+        "Laos", "Vientiane"
+    ],
+    [
+        "Lesoto", "Maseru"
+    ],
+    [
+        "Letônia", "Riga"
+    ],
+    [
+        "Líbano", "Beirute"
+    ],
+    [
+        "Libéria", "Monróvia"
+    ],
+    [
+        "Líbia", "Trípoli"
+    ],
+    [
+        "Listenstaine", "Vaduz"
+    ],
+    [
+        "Lituânia", "Vilnius"
+    ],
+    [
+        "Luxemburgo", "Luxemburgo"
+    ],
+    [
+        "Macedónia do Norte", "Skopje"
+    ],
+    [
+        "Madagáscar", "Antananarivo"
+    ],
+    [
+        "Malásia", "Kuala Lumpur"
+    ],
+    [
+        "Maláui", "Lilongwe"
+    ],
+    [
+        "Maldivas", "Malé"
+    ],
+    [
+        "Mali", "Bamaco"
+    ],
+    [
+        "Malta", "La Valeta"
+    ],
+    [
+        "Marrocos", "Rabat"
+    ],
+    [
+        "Mauritânia", "Nouakchott"
+    ],
+    [
+        "Maurícia", "Port Louis"
+    ],
+    [
+        "México", "Cidade do México"
+    ],
+    [
+        "Micronésia", "Paliquir"
+    ],
+    [
+        "Moçambique", "Maputo"
+    ],
+    [
+        "Moldávia", "Quixinau"
+    ],
+    [
+        "Mónaco", "Mónaco"
+    ],
+    [
+        "Mongólia", "Ulã Bator"
+    ],
+    [
+        "Montenegro", "Podgorica"
+    ],
+    [
+        "Myanmar (Birmânia)", "Naipyidó"
+    ],
+    [
+        "Namíbia", "Windhoek"
+    ],
+    [
+        "Nauru", "Yaren"
+    ],
+    [
+        "Nepal", "Catmandu"
+    ],
+    [
+        "Nicarágua", "Manágua"
+    ],
+    [
+        "Níger", "Niamei"
+    ],
+    [
+        "Nigéria", "Abuja"
+    ],
+    [
+        "Noruega", "Oslo"
+    ],
+    [
+        "Nova Zelândia", "Wellington"
+    ],
+    [
+        "Omã", "Mascate"
+    ],
+    [
+        "Palau", "Ngerulmud"
+    ],
+    [
+        "Panamá", "Cidade do Panamá"
+    ],
+    [
+        "Papua-Nova Guiné", "Port Moresby"
+    ],
+    [
+        "Paquistão", "Islamabad"
+    ],
+    [
+        "Paraguai", "Assunção"
+    ],
+    [
+        "Peru", "Lima"
+    ],
+    [
+        "Polónia", "Varsóvia"
+    ],
+    [
+        "Portugal", "Lisboa"
+    ],
+    [
+        "Quénia", "Nairobi"
+    ],
+    [
+        "Quirguistão", "Biskeque"
+    ],
+    [
+        "Quiribati", "Taraua"
+    ],
+    [
+        "Reino Unido", "Londres"
+    ],
+    [
+        "República Centro-Africana", "Bangui"
+    ],
+    [
+        "República Checa", "Praga"
+    ],
+    [
+        "República Democrática do Congo", "Quinxassa"
+    ],
+    [
+        "República do Congo", "Brazavile"
+    ],
+    [
+        "República Dominicana", "Santo Domingo"
+    ],
+    [
+        "Roménia", "Bucareste"
+    ],
+    [
+        "Ruanda", "Quigali"
+    ],
+    [
+        "Rússia", "Moscovo"
+    ],
+    [
+        "Salomão", "Honiara"
+    ],
+    [
+        "Salvador", "São Salvador"
+    ],
+    [
+        "Samoa", "Apia"
+    ],
+    [
+        "Santa Lúcia", "Castries"
+    ],
+    [
+        "São Cristóvão e Nevis", "Basseterre"
+    ],
+    [
+        "São Marinho", "São Marinho"
+    ],
+    [
+        "São Tomé e Príncipe", "São Tomé"
+    ],
+    [
+        "São Vicente e Granadinas", "Kingstown"
+    ],
+    [
+        "Seicheles", "Vitória"
+    ],
+    [
+        "Senegal", "Dacar"
+    ],
+    [
+        "Serra Leoa", "Freetown"
+    ],
+    [
+        "Sérvia", "Belgrado"
+    ],
+    [
+        "Singapura", "Singapura"
+    ],
+    [
+        "Síria", "Damasco"
+    ],
+    [
+        "Somália", "Mogadíscio"
+    ],
+    [
+        "Sri Lanca", "Sri Jayawardenapura Kotte"
+    ],
+    [
+        "Suazilândia", "Mbabane"
+    ],
+    [
+        "Sudão", "Cartum"
+    ],
+    [
+        "Sudão do Sul", "Juba"
+    ],
+    [
+        "Suécia", "Estocolmo"
+    ],
+    [
+        "Suíça", "Berna"
+    ],
+    [
+        "Suriname", "Paramaribo"
+    ],
+    [
+        "Tailândia", "Banguecoque"
+    ],
+    [
+        "Tajiquistão", "Duchanbe"
+    ],
+    [
+        "Tanzânia", "Dodoma"
+    ],
+    [
+        "Timor-Leste", "Díli"
+    ],
+    [
+        "Togo", "Lomé"
+    ],
+    [
+        "Tonga", "Nucualofa"
+    ],
+    [
+        "Trinidad e Tobago", "Porto de Espanha"
+    ],
+    [
+        "Tunísia", "Tunes"
+    ],
+    [
+        "Turcomenistão", "Asgabate"
+    ],
+    [
+        "Turquia", "Ancara"
+    ],
+    [
+        "Tuvalu", "Funafuti"
+    ],
+    [
+        "Ucrânia", "Kiev"
+    ],
+    [
+        "Uganda", "Campala"
+    ],
+    [
+        "Uruguai", "Montevidéu"
+    ],
+    [
+        "Usbequistão", "Tashkent"
+    ],
+    [
+        "Vanuatu", "Port Vila"
+    ],
+    [
+        "Vaticano", "Cidade do Vaticano"
+    ],
+    [
+        "Venezuela", "Caracas"
+    ],
+    [
+        "Vietnã", "Hanói"
+    ],
+    [
+        "Zâmbia", "Lusaca"
+    ],
+    [
+        "Zimbábue", "Harar"
+    ]
+];
